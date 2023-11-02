c2ae51f4ee1a5f2b79f57c73d00023c8963d6889
Shahab Ziaeian <shahabziaeian@yahoo.de>
2023-11-02 12:25:39 +0100

    finishedTask

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 import { createServer } from "node:http";
-
-// export const server = …
+import Chance from "chance";
+const chance = new Chance();
+const name = chance.name();
+const age = chance.age();
+const profession = chance.profession();
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.end(
+      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    );
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
+  }
+});
